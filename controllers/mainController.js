5af2081e424c5b89a08acde1da181279158fb830
Max I <clickkava@gmail.com>
2024-04-28 23:04:29 +0300

    Add form with secret code to become admin

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -28,16 +28,31 @@ exports.main_profile = asyncHandler(async (req, res, next) => {
 
 // Handle profile form POST
 exports.main_profile_post = asyncHandler(async (req, res, next) => {
-  if (req.body.secretcode === `${req.user.first_name}${req.user.last_name}5000`) {
-    await User.findByIdAndUpdate(req.user._id, { membership_status: true });
-    res.redirect("/profile");
-  } else {
-    res.render("profile", {
-      title: "Members Only - Profile",
-      user: req.user,
-      errors: ["Wrong secret code."],
-    });
-  }
+  if (req.body.formType === "updateMembership") {
+    if (req.body.secretcode === `${req.user.first_name}${req.user.last_name}5000`) {
+      await User.findByIdAndUpdate(req.user._id, { membership_status: true });
+      res.redirect("/profile");
+    } else {
+      res.render("profile", {
+        title: "Members Only - Profile",
+        user: req.user,
+        errors: ["Wrong secret code."],
+      });
+    }
+  };
+
+  if (req.body.formType === "updateAdmin") {
+    if (req.body.secretadmincode === `${req.user.first_name}${req.user.last_name}7000`) {
+      await User.findByIdAndUpdate(req.user._id, { membership_status: true, admin: true });
+      res.redirect("/profile");
+    } else {
+      res.render("profile", {
+        title: "Members Only - Profile",
+        user: req.user,
+        errors: ["Wrong secret code."],
+      });
+    }
+  };
 });
 
 // Display sign up form on GET
